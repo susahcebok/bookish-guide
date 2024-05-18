b54da975b80dd171083800e98f3d83a1783ed7f9
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2024-05-18 13:39:21 +0700

    Create share.js

---
+++ public/js/share.js
@@ -0,0 +1,18 @@
+let shareData = {
+        title: 'Couple Twibbon',
+        text: 'Web Generated Couple Pictures',
+        url: 'https://savegazza.netlify.app/',
+      }
+
+      const btn = document.querySelector('span');
+      const resultPara = document.querySelector('.result');
+
+      btn.addEventListener('click', () => {
+        navigator.share(shareData)
+          .then(() =>
+            resultPara.textContent = 'Couple Twibbon shared successfully'
+          )
+          .catch((e) =>
+            resultPara.textContent = 'Error: Share canceled!'
+          )
+      });
